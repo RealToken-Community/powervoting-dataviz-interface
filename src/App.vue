c64ae35746439d291bf7719fa9efeb9290920116
Yohann76 <yohanndurand76@gmail.com>
2026-03-04 09:25:31 +0000

    feat: remove analysis link in nav

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,7 @@ const { t } = useI18n()
             <RouterLink to="/history">{{ t('nav.history') }}</RouterLink>
             <RouterLink to="/generate">{{ t('nav.generate') }}</RouterLink>
             <RouterLink to="/upload">{{ t('nav.upload') }}</RouterLink>
-            <RouterLink to="/analysis">{{ t('nav.analysis') }}</RouterLink>
+            <!-- <RouterLink to="/analysis">{{ t('nav.analysis') }}</RouterLink> -->
             <span class="lang-switcher">
               <button type="button" class="lang-btn" :class="{ active: $i18n.locale === 'en' }" @click="setLocale('en')">{{ t('nav.langEn') }}</button>
               <span class="lang-sep">|</span>
